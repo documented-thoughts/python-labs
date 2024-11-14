680d77f4f61b5dc2df21e887352044518047f141
DocumentedThoughts <askhat.zilmukhamedov@gmail.com>
2024-11-13 17:10:10 -0700

    Update basics.py

--- src/basics.py
+++ src/basics.py
@@ -24,4 +24,31 @@ dinner = "turkey sandwich"
 
 print(breakfast, dinner, sep=", ") # omlet, turkey sandwich
 
-# page 74
+phrase = 'He said, "You are one of a kind."' 
+print(phrase) # He said, "You are one of a kind."
+
+phrase = 'He said, \"You are one of a kind.\"'
+print(phrase) # He said, "You are one of a kind."
+
+x = "apple"
+y = "apple"
+
+print(id(x))  # 2516685353968
+print(id(y))  # 2516685353968
+print(x == y) # True
+
+phrase = "one"
+print(phrase + ", two, three") # one, two, three
+print("{}, two, three".format(phrase)) # one, two, three
+print(f"{phrase}, two, three") # one, two, three
+
+drink = "espresso"
+print(drink[0]) # e
+print(drink[1]) # s
+print(drink[2]) # p
+print(drink[3]) # r
+print(drink[4]) # e
+print(drink[5]) # s
+print(drink[6]) # s
+print(drink[7]) # 0
+# print(drink[8]) IndexError: string index out of range 
